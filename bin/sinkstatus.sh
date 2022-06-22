8830cbfc0d01ad18329849132afc5898249d80fe
vkyriakakis <39331645+vkyriakakis@users.noreply.github.com>
2022-06-22 17:13:46 +0300

    Add files via upload

--- bin/sinkstatus.sh
+++ bin/sinkstatus.sh
@@ -6,24 +6,43 @@ headset="alsa_output.usb-Logitech_Inc._Logitech_USB_Headset_H340-00.analog-stere
 speakers="alsa_output.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_YDSB1730201001624B-00.analog-stereo"
 
 # Show the initial sink
-cur_sink=($(pactl list sinks short | grep RUNNING))
+pulse_info=($(pactl info | grep "Default Sink:"))
+cur_sink=${pulse_info[2]}
+cur_sink_entry=($(pactl list sinks short | grep $cur_sink))
+is_running=${cur_sink_entry[6]}
 
-if [ ${cur_sink[1]} = $headset ]; then
-	echo "%{F#d2c446}%{F-}"
-elif [ ${cur_sink[1]} = $speakers ]; then
-	echo "%{F#d2c446}%{F-}"
-else
-	echo "%{F#e1e0dc}%{F-}"
+if [ $cur_sink = $headset ]; then
+	if [ $is_running = "RUNNING" ]; then
+		echo "%{F#d2c446}%{F-}"
+	else
+		echo "%{F#e1e0dc}%{F-}"
+	fi
+elif [ $cur_sink = $speakers ]; then
+	if [ $is_running = "RUNNING" ]; then
+		echo "%{F#d2c446}%{F-}"
+	else
+		echo "%{F#e1e0dc}%{F-}"
+	fi
 fi
 
 # Wait for default sink change events
-pactl subscribe 2> /dev/null | grep --line-buffered "sink #" | while read -r line ; do
-	cur_sink=($(pactl list sinks short | grep RUNNING))
-	if [ ${cur_sink[1]} = $headset ]; then
-		echo "%{F#d2c446}%{F-}"
-	elif [ ${cur_sink[1]} = $speakers ]; then
-		echo "%{F#d2c446}%{F-}"
-	else
-		echo "%{F#e1e0dc}%{F-}"
+pactl subscribe 2> /dev/null | grep --line-buffered "sink" | while read -r line ; do
+	pulse_info=($(pactl info | grep "Default Sink:"))
+	cur_sink=${pulse_info[2]}
+	cur_sink_entry=($(pactl list sinks short | grep $cur_sink))
+	is_running=${cur_sink_entry[6]}
+
+	if [ $cur_sink = $headset ]; then
+		if [ $is_running = "RUNNING" ]; then
+			echo "%{F#d2c446}%{F-}"
+		else
+			echo "%{F#e1e0dc}%{F-}"
+		fi
+	elif [ $cur_sink = $speakers ]; then
+		if [ $is_running = "RUNNING" ]; then
+			echo "%{F#d2c446}%{F-}"
+		else
+			echo "%{F#e1e0dc}%{F-}"
+		fi
 	fi
 done
--- bin/sinktoggle.sh
+++ bin/sinktoggle.sh
@@ -6,10 +6,10 @@ headset="alsa_output.usb-Logitech_Inc._Logitech_USB_Headset_H340-00.analog-stere
 speakers="alsa_output.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_YDSB1730201001624B-00.analog-stereo"
 
 # Find the current sink and toggle it
-cur_sink=($(pactl list sinks short | grep RUNNING))
+cur_sink=($(pactl info | grep "Default Sink:"))
 
-if [ ${cur_sink[1]} = $headset ]; then
+if [ ${cur_sink[2]} = $headset ]; then
 	pacmd set-default-sink $speakers
-elif [ ${cur_sink[1]} = $speakers ]; then
+elif [ ${cur_sink[2]} = $speakers ]; then
 	pacmd set-default-sink $headset
 fi
